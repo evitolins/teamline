a84d0af7f4860bf67fbfd71473e4ee059f27d06d
Eriks Vitolins <evitolins@gmail.com>
2016-06-28 14:33:33 -0700

    Now showing time ranges

--- main.js
+++ main.js
@@ -16,6 +16,17 @@ var in_some_ranges = function (val, ranges) {
 };
 var sleep_ranges = [[0,6], [22,24]];
 
+var generate_offset_ranges = function (val, pre, post) {
+    var r=[], v, i;
+    for (i=pre; i<post; i++) {
+      v = (val+i) % 24;
+      if (v < 0) v = (24000 + v) % 24;
+      r.push(v);
+    }
+    return r;
+};
+
+
 var users = [
     new User('mick', "Australia/Sydney"),
     new User('jane', "America/Los_Angeles"),
@@ -25,6 +36,9 @@ var users = [
     new User('carol', "Asia/Kuala_Lumpur"),
     new User('miko', "Asia/Tokyo")
 ];
+var colors = ['#222', '#719dd6', '#84d648', '#5c9631', '#7c49ca'];
+var color_info = ['sleep', 'early', 'workday', 'after hours', 'late'];
+
 var i,n,t,h,X;
 
 var avatars = [
@@ -49,17 +63,36 @@ for (i=0; i<users.length; i++) {
     users[i].avatar = avatars[i];
     users[i].hour = h;
     users[i].time = t.format("dddd, ha");
+    users[i].hours = generate_offset_ranges(h, -12, 12);
 }
 
-
 Vue.filter('time_color', function (value) {
-    var colors = ['#446', '#cc4', '#60c', '#46f'];
     if (value >= 0 && value < 6) return colors[0];
-    if (value >= 6 && value < 12) return colors[1];
-    if (value >= 12 && value < 18) return colors[2];
-    if (value >= 18 && value < 24) return colors[3];
+    if (value >= 6 && value < 9) return colors[1];
+    if (value >= 9 && value < 18) return colors[2];
+    if (value >= 18 && value < 21) return colors[3];
+    if (value >= 21 && value < 24) return colors[4];
+    if (value == 24) return colors[0];
 });
 
+var build_swatches = function () {
+    var i;
+    for(i=0; i<colors.length; i++) {
+        elm = document.createElement('div');
+        elm.style.fontSize = '0.5em';
+        elm.style.width = '50px';
+        elm.style.height = '2em';
+        elm.style.color = '#fff';
+        elm.style.padding = '0px 8px';    
+        elm.style.backgroundColor = colors[i];
+        elm.style.borderRadius = '20px';
+        elm.style.lineHeight = '2em';
+        elm.innerHTML = color_info[i];
+        document.body.appendChild(elm);
+    }
+};
+
+build_swatches();
 
 new Vue({
   el: '#app',
