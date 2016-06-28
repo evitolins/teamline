8887d2a1c7ce1b933f0d05396954ddfa81e3953d
Eriks Vitolins <evitolins@gmail.com>
2016-06-28 16:47:39 -0700

    Code cleanup and style tweaks

--- main.js
+++ main.js
@@ -5,18 +5,8 @@ var User = function (name, timezone) {
 var in_range = function (val, range) {
     return (val >= range[0] && val <= range[1] );
 };
-var in_some_ranges = function (val, ranges) {
-    var i;
-    for (i=0; i<ranges.length; i++) {
-        if (in_range(val, ranges[i])) {
-            return true;
-        }
-    }
-    return false;
-};
-var sleep_ranges = [[0,6], [22,24]];
 
-var generate_offset_ranges = function (val, pre, post) {
+var generate_offset_hours = function (val, pre, post) {
     var r=[], v, i;
     for (i=pre; i<post; i++) {
       v = (val+i) % 24;
@@ -28,13 +18,14 @@ var generate_offset_ranges = function (val, pre, post) {
 
 
 var users = [
-    new User('mick', "Australia/Sydney"),
-    new User('jane', "America/Los_Angeles"),
+    new User('jimmy', "America/Los_Angeles"),
+    new User('jane', "America/San_Francisco"),
     new User('frank', "America/New_York"),
     new User('bobby', "Europe/London"),
     new User('fred', "Europe/Riga"),
-    new User('carol', "Asia/Kuala_Lumpur"),
-    new User('miko', "Asia/Tokyo")
+    new User('carol', "Europe/Sofia"),
+    new User('miko', "Asia/Tokyo"),
+    new User('mick', "Australia/Sydney")
 ];
 var colors = ['#222', '#719dd6', '#84d648', '#5c9631', '#7c49ca'];
 var color_info = ['sleep', 'early', 'workday', 'after hours', 'late'];
@@ -49,6 +40,7 @@ var avatars = [
     'https://randomuser.me/api/portraits/women/85.jpg',
     'https://randomuser.me/api/portraits/women/95.jpg',
     'https://randomuser.me/api/portraits/women/98.jpg',
+    'https://randomuser.me/api/portraits/women/99.jpg',
 ];
 
 moment.locale('en');
@@ -59,11 +51,10 @@ for (i=0; i<users.length; i++) {
     n = users[i].name;
     t = currentLoc.clone().tz(users[i].timezone);
     h = parseInt(t.format('H'), 10);
-    X = (in_some_ranges(h, sleep_ranges)) ? '* SLEEP' : '';
     users[i].avatar = avatars[i];
     users[i].hour = h;
     users[i].time = t.format("dddd, ha");
-    users[i].hours = generate_offset_ranges(h, -12, 12);
+    users[i].hours = generate_offset_hours(h, -12, 12);
 }
 
 Vue.filter('time_color', function (value) {
@@ -75,20 +66,16 @@ Vue.filter('time_color', function (value) {
     if (value == 24) return colors[0];
 });
 
+
+var elem_footer = document.getElementById('footer');
 var build_swatches = function () {
     var i;
     for(i=0; i<colors.length; i++) {
         elm = document.createElement('div');
-        elm.style.fontSize = '0.5em';
-        elm.style.width = '50px';
-        elm.style.height = '2em';
-        elm.style.color = '#fff';
-        elm.style.padding = '0px 8px';    
+        elm.className = 'key_pill';
         elm.style.backgroundColor = colors[i];
-        elm.style.borderRadius = '20px';
-        elm.style.lineHeight = '2em';
         elm.innerHTML = color_info[i];
-        document.body.appendChild(elm);
+        elem_footer.appendChild(elm);
     }
 };
 
